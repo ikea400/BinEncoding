153a6b781c6396247068611776d8450dcd1a59ad
NLavoie <nataellavoie@gmail.com>
2025-10-20 19:25:43 -0400

    Finalisation de hex encode/decode

--- BinEncoding/Source/BinEncoding.h
+++ BinEncoding/Source/BinEncoding.h
@@ -5,41 +5,94 @@
 #include <span>
 #include <algorithm>
 #include <string_view>
+#include <vector>
 
 namespace ikea400::Bin {
 
-	inline constexpr const std::string_view kHexFormat = "hex:";
-
-	std::string prepareFormat(const std::string_view format, std::size_t size) {
-		std::string value(format.size() + size, '\0');
-		std::copy(format.begin(), format.end(), value.begin());
-		return value;
-	}
-
-	template <typename T>
-	std::string EncodeHex(const T& value)
+	namespace Detail
 	{
-		using value_type = T::value_type;
-		std::string output(value.size() * sizeof(value_type) * 2, '\0');
+		inline constexpr const std::string_view kHexFormat = "hex:";
+
+		inline std::string PrepareFormat(const std::string_view format, std::size_t size) {
+			std::string value(format.size() + size, '\0');
+			std::copy(format.begin(), format.end(), value.begin());
+			return value;
+		}
+
+		template <typename T>
+		inline void EncodeHex(std::string_view dest, const T& value)
+		{
 #if defined(__AVX2__)
-		encodeHexVec((uint8_t*)output.data(), data, size);
+			encodeHexVec((uint8_t*)dest.data(), (const uint8_t*)value.data(), value.size() * sizeof(T::value_type));
 #else
-		encodeHex((uint8_t*)output.data(), value.data(), value.size() * sizeof(value_type));
+			encodeHex((uint8_t*)dest.data(), value.data(), value.size() * sizeof(T::value_type));
 #endif
-		return output;
-	}
-	;
-	template <typename T>
-	std::string FormatHex(const T& value)
+		}
+
+
+		inline void DecodeHex(std::vector<uint8_t>& dest, const std::string_view value)
+		{
+#if defined(__AVX2__)
+			decodeHexVec((uint8_t*)dest.data(), (const uint8_t*)value.data(), value.size());
+#else
+			decodeHexLUT4((uint8_t*)dest.data(), (const uint8_t*)value.data(), value.size());
+#endif
+		}
+	} // namespace Detail
+
+	namespace Hex
 	{
-		using value_type = T::value_type;
-		std::string output = prepareFormat(kHexFormat, value.size() * sizeof(T::value_type) * 2);
-		
-#if defined(__AVX2__)
-		encodeHexVec((uint8_t*)output.data(), data, size);
-#else
-		encodeHex((uint8_t*)output.data() + kHexFormat.size(), value.data(), value.size() * sizeof(value_type));
-#endif
-		return output;
-	}
-}
+		template <typename T>
+		inline std::string Encode(const T& value)
+		{
+			using value_type = T::value_type;
+			std::string output(value.size() * sizeof(value_type) * 2, '\0');
+			if (!value.empty())
+				Detail::EncodeHex(output, value);
+			return output;
+		}
+
+		template <typename T>
+		inline std::string Format(const T& value)
+		{
+			using value_type = T::value_type;
+			std::string output = Detail::PrepareFormat(Detail::kHexFormat, value.size() * sizeof(T::value_type) * 2);
+			if (!value.empty()) 
+				Detail::EncodeHex(std::string_view(output).substr(Detail::kHexFormat.size()), value);
+			return output;
+		}
+
+		template <bool Throw = true>
+		inline std::vector<uint8_t> DecodeFormat(std::string_view value)
+		{
+			if (!value.starts_with(Detail::kHexFormat)) {
+				if constexpr (Throw)
+					throw std::exception("Hex decode format: invalid format prefix");
+				
+				return {};
+			}
+			return Decode<Throw>(value.substr(Detail::kHexFormat.size()));
+		}
+
+		template <bool Throw = true>
+		inline std::vector<uint8_t> Decode(std::string_view value)
+		{
+			if (value.empty()) return {};
+			if (value.size() % 2 != 0) {
+				if constexpr (Throw)
+					throw std::exception("Hex decode: input size is not even");
+				
+				return {};
+			}
+
+			std::vector<uint8_t> output(value.size() / 2);
+
+			Detail::DecodeHex(output, value);
+
+			return output;
+		}
+
+	} // namespace Hex
+
+
+} // namespace ikea400::Bin
--- Example/Source/main.cpp
+++ Example/Source/main.cpp
@@ -7,6 +7,17 @@ int main()
 {
 	std::array data = { uint8_t{0xDE}, uint8_t{0xAD}, uint8_t{0xBE}, uint8_t{0xEF} };
 
-	std::cout << ikea400::Bin::FormatHex(data) << std::endl;
+	std::string encoded = ikea400::Bin::Hex::Encode(data);
+	std::cout << encoded << std::endl;
+
+	std::string formated = ikea400::Bin::Hex::Format(data);
+	std::cout << formated << std::endl;
+
+	auto decoded = ikea400::Bin::Hex::Decode<true>(encoded);
+	std::string recoded = ikea400::Bin::Hex::Encode(decoded);
+
+	std::cout << recoded << std::endl;
+
+
 	return 0;
 }
